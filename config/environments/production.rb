8b04b4269a08f65d3c1d66433a8b989e8c5de952
rodrigo.toledo <rodrigo.toledo@creditcorp.com.br>
2022-06-21 22:51:10 -0300

    disable actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.web_socket_server_url = "wss://the-music-album.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = [ 'https://the-music-album.herokuapp.com', /http:\/\/the-music-album.herokuapp.com.*/ ]
-  config.assets.compile = true
+  # config.web_socket_server_url = "wss://the-music-album.herokuapp.com/cable"
+  # config.action_cable.allowed_request_origins = [ 'https://the-music-album.herokuapp.com', /http:\/\/the-music-album.herokuapp.com.*/ ]
+  # config.assets.compile = true
 end
