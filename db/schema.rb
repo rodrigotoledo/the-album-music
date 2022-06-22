7c36ec766206bf71a7fed10a25602ad86ae5e466
rodrigo.toledo <rodrigo.toledo@creditcorp.com.br>
2022-06-22 01:50:06 -0300

    authentication with csrf token

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2022_06_22_020530) do
   create_table "artists", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "user_id", null: false
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_artists_on_user_id"
